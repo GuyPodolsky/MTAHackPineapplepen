c6b8419cfcb9282e24d1fa20bdda794994edfbb3
ofeks96 <g2ofek@gmail.com>
2021-05-06 17:46:46 +0300

    user and default pic

---
+++ Server/src/DiscussionManager.java
@@ -0,0 +1,13 @@
+public class DiscussionManager {
+
+    private Map <Integer, Idea> allIdeas; //Id + idea hashmap
+    private Idea currTopic;
+    private DiscussionFlowSummary tree;
+
+    DiscussionManager (Idea main) {
+
+    }
+
+    public String PresentProblem(String str) {   }
+
+}
