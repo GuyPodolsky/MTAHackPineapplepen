b5019a4db90c3ae80161aa69bd09bd0cb0068fdd
Tom Weinberg <tomwe@mta.ac.il>
2021-05-07 07:26:49 +0300

    Finished Client Threads

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -20,7 +20,9 @@ public class Client {
         DE = _DE;
         hostAddress = new InetSocketAddress(hostname, port);
         Thread clientSendThread = new Thread(new ClientSendThread(sendData, hostAddress));
-        Thread clientRecvThread = new Thread(new ClientReceiveThread(recvData, hostAddress));
+        Thread clientRecvThread = new Thread(new ClientReceiveThread(DE, hostAddress));
+        clientSendThread.start();
+        clientRecvThread.start();
     }
 
     public void send(String msg) {
@@ -30,64 +32,8 @@ public class Client {
         }
     }
 
-    public void HandleClientEvent(String protCmd) {
-        String strArr[];
-        if (protCmd.length() >= 3) {
-            switch (protCmd.substring(0, 3)) {
-                case "AID":
-                    strArr = protCmd.substring(4).split(" ");
-                    handleAddIdea(strArr[1], Integer.parseInt(strArr[0]));
-                    break;
-                case "LKE":
-                    handleLike(Integer.valueOf(protCmd.substring(4)));
-                    break;
-                case "DLK":
-                    handleDislike(Integer.valueOf(protCmd.substring(4)));
-                    break;
-                case "ACO":
-                    strArr = protCmd.substring(4).split(" ");
-                    handleMessage(strArr[1], Integer.parseInt(strArr[0]));
-                    break;
-                case "ACM":
-                    handleMessage(protCmd.substring(4), -1);
-                    break;
-                case "MID":
-                    handleMove(Integer.valueOf(protCmd.substring(4)));
-                    break;
-            }
-        }
-    }
-
-
-
-
     private synchronized void sendBuff(SocketChannel client, ByteBuffer buffer) throws IOException {
         client.write(buffer);
     }
 
-    public void handleLike(int id){
-        DE.setLikeById(id);
-    }
-
-    public void handleDislike(int id){
-        DE.setDisLikeById(id);
-    }
-
-    public void handleMessage(String message,int refID){
-        Message msg = new Message(message);
-        DE.sendMessage(msg,refID);
-    }
-
-    public void handleMove(int nodeID) {
-        IdeaNode newNode = DE.getDisTree().getParent();
-        if(nodeID != newNode.getID())
-            newNode = DE.getDisTree().getIdea(nodeID);
-        DE.setDisTree(newNode);
-    }
-
-    public void handleAddIdea(String idea, int genID) {
-        Idea newIdea = new Idea(idea,genID);
-        IdeaNode newIdeaNode = new IdeaNode(newIdea,DE.getDisTree());
-        DE.addIdea(newIdeaNode);
-    }
 }
